d4a867d76218ec8d0e9d0b4f970b02a378793840
DivyanshRawat15 <divyansh.rawat@hiddenbrains.in>
2026-04-13 18:15:17 +0530

    Day 7 files

---
+++ NodeJS/Day7/exp.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.get("/product", (req, res) => {
+  const { name, price } = req.query;
+  res.json({ name, price });
+});
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
